ab488ea351d96c0e1c2fdf955d0bee2af68d3da5
Tang_jy <1336288671@qq.com>
2019-07-11 21:00:59 +0800

    'feat:新增分页功能'

--- src/js/config.js
+++ src/js/config.js
@@ -4,13 +4,14 @@ require.config({
       jquery: 'libs/jquery/jquery-1.11.3.min',
       header: 'js/modules/header',
       footer: 'js/modules/footer',
-      template:'libs/art-template/template-web',
-      url:'js/modules/url',
-      zoom: 'libs/jquery-plugins/jquery.elevateZoom-3.0.8.min',
-      fly: 'libs/jquery-plugins/jquery.fly.min',
-      superslide:'libs/jquery/superslide.2.1',
-      banner:'js/modules/banner',
-      cookie:'libs/jquery-plugins/jquery.cookie'
+      template: 'libs/art-template/template-web',
+      url: 'js/modules/url',
+      zoom:  'libs/jquery-plugins/jquery.elevateZoom-3.0.8.min',
+      fly:  'libs/jquery-plugins/jquery.fly.min',
+      superslide: 'libs/jquery/superslide.2.1',
+      banner: 'js/modules/banner',
+      cookie: 'libs/jquery-plugins/jquery.cookie',
+      paging: 'libs/jquery-plugins/paging.min'
     },
     //垫片
     //不满足AMD规范，但又依赖别的模块
@@ -25,6 +26,9 @@ require.config({
         deps: ['jquery']
       },
       cookie:{
+        deps: ['jquery']
+      },
+      paging:{
         deps: ['jquery']
       }
     }
--- src/js/list.js
+++ src/js/list.js
@@ -1,33 +1,80 @@
 
 require(['./config'], () => {
-    //接收template模块,有返回值
-  require(['template','url','header','footer'], (template,url) => {
+  //接收template模块,有返回值
+  require(['template', 'url', 'header', 'paging','footer'], (template, url) => {
     //list 页面功能逻辑
-    class List{
-      constructor(){
+    class List {
+      constructor() {
         this.container = $('#shop-l')
-        this.getData().then((list) =>{
+        this.getData().then((list) => {
           //首先then里面接收了resolve传递过来的list数据，紧接着继续传递给renderList
           this.renderList(list)
+          this.goSort(list)
+          this.goPaging(list)
         })
       }
-      getData () {
-        return new Promise(resolve =>{
-          $.get(url.rapBaseUrl + '/shop/list',resp =>{
-            console.log(resp)
-            if(resp.code === 200){
+      getData() {
+        return new Promise(resolve => {
+          $.get(url.rapBaseUrl + '/shop/list', resp => {
+            //console.log(resp)
+            if (resp.code === 200) {
               //传递实参，把从接口取到的数据传给 then
               resolve(resp.body.list)
             }
           })
         })
       }
-      renderList(list){
+      renderList(list) {
         //第二个参数{list:list} 里面的key:指的是template里面需要的变量名，value指的是从接口获取到的数据
-        let str = template('list-template',{list})
+        let str = template('list-template', { list })
         this.container.html(str)
+      }
+      goSort(list) {
+        $('#sort').hover(() => {
+          $('#sort-u').show()
+          //升序
+          $('.li2').on('click', () =>{
+            list = list.sort(function (a, b) {
+              return a.price - b.price
+            })
+            let str = template('list-template', { list })
+            this.container.html(str)
+            $('.moren').html($('.li2').html())
+          })
+          //降序
+          $('.li3').on('click', () =>{
+            list = list.sort(function (a, b) {
+              return b.price - a.price
+            })
+            let str = template('list-template', { list })
+            this.container.html(str)
+            $('.moren').html($('.li3').html())
+          })
+        }, () => {
+          $('#sort-u').hide()
+        })
+      }
+      goPaging (list) {
+        var setTotalCount = 120;
+        $('#box').paging({
+            initPageNo: 1, // 初始页码
+            totalPages: 10, //总页数
+            totalCount: '合计' + setTotalCount + '条数据', // 条目总数
+            slideSpeed: 600, // 缓动速度。单位毫秒
+            jump: true, //是否支持跳转
+            callback: function(page) { // 回调函数
+                console.log(page);
+                //console.log(list)
+                console.log(list.length)
+                list = list.map(item =>{
+                   item.id += list.length
+                   return item
+                })
+                console.log(list)
+            }
+        })
       }
     }
     new List()
-    })
   })
+})
